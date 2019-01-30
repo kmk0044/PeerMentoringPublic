67fe4137b73ac6d6ad3824893dfdf4d03cd0d214
adf0018 <35674977+adf0018@users.noreply.github.com>
2019-01-30 12:25:37 -0600

    programs page updated

--- programs.php
+++ programs.php
@@ -1,26 +1,61 @@
-<!doctype html>
 <!DOCTYPE html>
 <html>
-<head>
-	<title>Programs - Peer Mentoring</title>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="/peermentoring/styleScript.css">
-</head>
-<body>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Programs - Peer Mentoring</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="styleScript.css">
+  </head>
+  <body>
+    <header>
+      <?php
+        include "header.html";
+      ?>
+    </header>
     <div>
-        <?php
-        include "header.php";
-    ?>
+      <br>
+      <img alt="bannerHolder" src="bannerHolder.png" style="padding:5pt;height: 275px; width:100%; display: block;height:2000">
+      <br>
     </div>
-    <div class="container">
-  <div class="row">
-    <div style="background-color: lightblue" class="col-sm">
-      One of three columns
+    <div class="container" style="padding:none;">
+      <div class="row">
+        <div class="card" style="width:22.5rem;padding:10pt;margin-right:15pt;margin-bottom:15pt;display:inline-block">
+          <img src="thumbnail.png" class="card-img-top" alt="100%x200">
+          <div class="card-body">
+            <h5 class="card-title">Program title</h5>
+            <p class="card-text">Some quick example text to preview the program and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary" style="border-style:none;">Join</a> <!-- link to external organization's homepage -->
+            <a href="#" class="btn btn-primary" style="border-style:none;">Learn more</a>
+          </div>
+        </div>
+        <div class="card" style="width:22.5rem;padding:10pt;margin-right:15pt;margin-bottom:15pt;">
+          <img src="thumbnail.png" class="card-img-top" alt="100%x200">
+          <div class="card-body">
+            <h5 class="card-title">Program title</h5>
+            <p class="card-text">Some quick example text to preview the program and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary" style="border-style:none;">Join</a> <!-- link to external organization's homepage -->
+            <a href="#" class="btn btn-primary" style="border-style:none;">Learn more</a>
+          </div>
+        </div>
+        <div class="card" style="width:22.5rem;padding:10pt;margin-right:15pt;margin-bottom:15pt;">
+          <img src="thumbnail.png" class="card-img-top" alt="100%x200">
+          <div class="card-body">
+            <h5 class="card-title">Program title</h5>
+            <p class="card-text">Some quick example text to preview the program and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary" style="border-style:none;">Join</a> <!-- link to external organization's homepage -->
+            <a href="#" class="btn btn-primary" style="border-style:none;">Learn more</a>
+          </div>
+        </div>
+        <div class="card" style="width:22.5rem;padding:10pt;margin-right:15pt;margin-bottom:15pt;">
+          <img src="thumbnail.png" class="card-img-top" alt="100%x200">
+          <div class="card-body">
+            <h5 class="card-title">Program title</h5>
+            <p class="card-text">Some quick example text to preview the program and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary" style="border-style:none;">Join</a> <!-- link to external organization's homepage (?)-->
+            <a href="#" class="btn btn-primary" style="border-style:none;">Learn more</a>
+          </div>
+        </div>
+      </div>
     </div>
-    <div style="background-color: grey" class="col-lg">
-      One of three columns
-    </div>
-  </div>
-</div>
-</body>
+  </body>
 </html>
