e6c6e939bc3bb554db2324518623689c7682e798
kmk0044 <46569244+kmk0044@users.noreply.github.com>
2019-02-06 13:41:37 -0600

    config fix attempt 2
    kk

--- config.php
+++ config.php
@@ -1,13 +1,24 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-   define('DB_SERVER', 'auburnpeermentoring.database.windows.net');
-   define('DB_USERNAME', 'peermentoring');
-   define('DB_PASSWORD', 'Group8!2019');
+   //define('DB_SERVER', 'auburnpeermentoring.database.windows.net');
+   //define('DB_USERNAME', 'peermentoring');
+   //define('DB_PASSWORD', 'Group8!2019');
  //  define('DB_NAME', 'kmk0044db');
  
+
+
+ $serverName = "auburnpeermentoring.database.windows.net"; // update me
+    $connectionOptions = array(
+        "Database" => "auburnpeermentoringdb", // update me
+        "Uid" => "peermentoring", // update me
+        "PWD" => "Group8!2019" // update me
+    );
+    //Establishes the connection
+    $link = sqlsrv_connect($serverName, $connectionOptions); 
+
 /* Attempt to connect to MySQL database */
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
+//$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
  
 // Check connection
 if($link === false){
