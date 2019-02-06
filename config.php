0338c5f93d0b844cdff60399da9bfef69698a150
kmk0044 <46569244+kmk0044@users.noreply.github.com>
2019-02-06 13:38:01 -0600

    modified config for Azure DB Connection
    Kate Kramer

--- config.php
+++ config.php
@@ -1,13 +1,13 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-   define('DB_SERVER', 'acadmysql.duc.auburn.edu');
-   define('DB_USERNAME', 'kmk0044');
-   define('DB_PASSWORD', 'Mkmm2013');
-   define('DB_NAME', 'kmk0044db');
+   define('DB_SERVER', 'auburnpeermentoring.database.windows.net');
+   define('DB_USERNAME', 'peermentoring');
+   define('DB_PASSWORD', 'Group8!2019');
+ //  define('DB_NAME', 'kmk0044db');
  
 /* Attempt to connect to MySQL database */
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
  
 // Check connection
 if($link === false){
