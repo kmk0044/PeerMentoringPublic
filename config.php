b34c4b4c35b59c3d4b5ad07a718971fd05213f0d
kate kramer <kate@katemariekramer.com>
2019-02-06 21:26:36 -0600

    working dir cycle1 - mysql
    kate

--- config.php
+++ config.php
@@ -1,24 +1,13 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-   //define('DB_SERVER', 'auburnpeermentoring.database.windows.net');
-   //define('DB_USERNAME', 'peermentoring');
-   //define('DB_PASSWORD', 'Group8!2019');
- //  define('DB_NAME', 'kmk0044db');
+   define('DB_SERVER', 'acadmysql.duc.auburn.edu');
+   define('DB_USERNAME', 'kmk0044');
+   define('DB_PASSWORD', 'Mkmm2013');
+   define('DB_NAME', 'kmk0044db');
  
-
-
- $serverName = "auburnpeermentoring.database.windows.net"; // update me
-    $connectionOptions = array(
-        "Database" => "auburnpeermentoringdb", // update me
-        "Uid" => "peermentoring", // update me
-        "PWD" => "Group8!2019" // update me
-    );
-    //Establishes the connection
-    $link = sqlsrv_connect($serverName, $connectionOptions); 
-
 /* Attempt to connect to MySQL database */
-//$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
  
 // Check connection
 if($link === false){
