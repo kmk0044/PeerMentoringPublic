558f39e80fbd003ca76b921c4aee811d25ec50c1
kate kramer <kate@katemariekramer.com>
2019-02-08 20:45:17 -0600

    azure attempt
    kate kramer

--- config.php
+++ config.php
@@ -1,16 +1,17 @@
 <?php
-/* Database credentials. Assuming you are running MySQL
-server with default setting (user 'root' with no password) */
-   define('DB_SERVER', 'acadmysql.duc.auburn.edu');
-   define('DB_USERNAME', 'kmk0044');
-   define('DB_PASSWORD', 'Mkmm2013');
-   define('DB_NAME', 'kmk0044db');
- 
-/* Attempt to connect to MySQL database */
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
- 
-// Check connection
-if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
-}
+
+	// PHP Data Objects(PDO) Sample Code:
+	try {
+	$conn = new PDO("sqlsrv:server = tcp:auburnpeermentoring.database.windows.net,1433; Database = auburnpeermentoringdb", "peermentor", "Group8!2019");
+	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	}
+	catch (PDOException $e) {
+	print("Error connecting to SQL Server.");
+	die(print_r($e));
+	}
+
+	// SQL Server Extension Sample Code:
+	$connectionInfo = array("UID" => "peermentor@auburnpeermentoring", "pwd" => "Group8!2019", "Database" => "auburnpeermentoringdb", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+	$serverName = "tcp:auburnpeermentoring.database.windows.net,1433";
+	$conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
